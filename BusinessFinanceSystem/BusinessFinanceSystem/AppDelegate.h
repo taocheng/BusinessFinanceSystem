aa7e320a06cf00ede001fdcacbb9a8e656264b13
taocheng <taocheng@QingQing-iOSdeMacBook-Pro.local>
2016-06-27 23:49:40 +0800

    新建企业财务系统工程

---
+++ BusinessFinanceSystem/BusinessFinanceSystem/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BusinessFinanceSystem
+//
+//  Created by taocheng on 16/6/27.
+//  Copyright © 2016年 taocheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BusinessFinanceSystem/BusinessFinanceSystem/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BusinessFinanceSystem
+//
+//  Created by taocheng on 16/6/27.
+//  Copyright © 2016年 taocheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
