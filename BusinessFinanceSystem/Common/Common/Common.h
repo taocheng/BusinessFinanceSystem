c90a64bdc597db19c823247ee07672e845f19ced
taocheng <taocheng@QingQing-iOSdeMacBook-Pro.local>
2016-06-28 00:17:40 +0800

    1.创建子工程

---
+++ BusinessFinanceSystem/Common/Common/Common.h
@@ -0,0 +1,13 @@
+//
+//  Common.h
+//  Common
+//
+//  Created by taocheng on 16/6/28.
+//  Copyright © 2016年 taocheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Common : NSObject
+
+@end
---
+++ BusinessFinanceSystem/Common/Common/Common.m
@@ -0,0 +1,13 @@
+//
+//  Common.m
+//  Common
+//
+//  Created by taocheng on 16/6/28.
+//  Copyright © 2016年 taocheng. All rights reserved.
+//
+
+#import "Common.h"
+
+@implementation Common
+
+@end
